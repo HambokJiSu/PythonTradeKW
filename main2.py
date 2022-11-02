240ace5fee323f237efa52b49a05f3306ed37b1b
unknown <truemindy@gmail.com>
2022-11-02 23:11:47 +0900

    First Commit

---
+++ main2.py
@@ -0,0 +1,30 @@
+from pykiwoom.kiwoom import *
+
+kiwoom = Kiwoom()
+kiwoom.CommConnect(block=True)
+
+#■■■■사용자 정보 확인 Start■■■■
+account_num = kiwoom.GetLoginInfo("ACCOUNT_CNT")        # 전체 계좌수
+accounts = kiwoom.GetLoginInfo("ACCNO")                 # 전체 계좌 리스트
+user_id = kiwoom.GetLoginInfo("USER_ID")                # 사용자 ID
+user_name = kiwoom.GetLoginInfo("USER_NAME")            # 사용자명
+keyboard = kiwoom.GetLoginInfo("KEY_BSECGB")            # 키보드보안 해지여부
+firewall = kiwoom.GetLoginInfo("FIREW_SECGB")           # 방화벽 설정 여부
+
+print(account_num)
+print(accounts)
+print(user_id)
+print(user_name)
+print(keyboard)
+print(firewall)
+#■■■■사용자 정보 확인 End■■■■
+
+#■■■■종목 코드 얻기 Start■■■■
+kospi = kiwoom.GetCodeListByMarket('0')
+kosdaq = kiwoom.GetCodeListByMarket('10')
+etf = kiwoom.GetCodeListByMarket('8')
+
+print(len(kospi), kospi)
+print(len(kosdaq), kosdaq)
+print(len(etf), etf)
+#■■■■종목 코드 얻기 End■■■■
